9efcfaad6cd444721a35633cbbbf843b0f18e23b
Pyuzya <73394151+Pyuzya@users.noreply.github.com>
2023-02-27 22:38:46 +0500

    first commit

---
+++ Основы/variable/js.js
@@ -0,0 +1,18 @@
+// 1
+let admin, name;
+
+name = 'Джон';
+
+admin = name;
+
+alert(admin);
+
+// 2
+
+let earth;
+let user;
+
+// 3
+
+// Для age не нужно, вычисляется после загрузки
+// Для birthday можно, оно постоянно и до загрузки
